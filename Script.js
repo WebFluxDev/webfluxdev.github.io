d582f2303608eda77fcdd64e22c6f0fcfc63ab22
webfluxinnovations.github.io <Webfluxinnovation@gmail.com>
2025-08-26 19:15:01 +0200

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('.btn').forEach(button => {
-        button.addEventListener('click', () => {
-            button.classList.add('clicked');
-            setTimeout(() => button.classList.remove('clicked'), 200);
-        });
-    });
-});
